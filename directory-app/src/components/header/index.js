1a29a4449ef8f7d64f32269345c7495b505776d2
SeanW <sean.walmer@outlook.com>
2020-07-26 12:51:42 -0400

    adding some basic header files as well as a json for my crew data.

---
+++ directory-app/src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+function Header() {
+  return (
+      <div>
+          <h1 className="center-align header">Check out the employee Directory!</h1>
+      </div>
+  );
+}
+
+export default Header;
