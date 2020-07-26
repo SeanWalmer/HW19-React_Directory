c3f84ec3c42ea1e79b2efa3fd556f633d46f4dff
SeanW <sean.walmer@outlook.com>
2020-07-26 13:19:51 -0400

    updated app to have both main and header in it. Main will contain the data and filtering tools

--- directory-app/src/App.js
+++ directory-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Header from './components/header/'
+import Main from './components/Main'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
