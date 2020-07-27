fe44b00ac97ac63771cded10b47731af3b8f3826
SeanW <sean.walmer@outlook.com>
2020-07-26 21:31:26 -0400

    Updates

---
+++ precache-manifest.bcfe9910c5461e5486ab90b1c1a1f875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c36f97e7cf18f7e374bda8caa0dc11",
+    "url": "/HW19-React_Directory/index.html"
+  },
+  {
+    "revision": "a95650069e11bbb535a4",
+    "url": "/HW19-React_Directory/static/css/main.d89f1689.chunk.css"
+  },
+  {
+    "revision": "f6a898080db71da9bf3a",
+    "url": "/HW19-React_Directory/static/js/2.8f8731de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW19-React_Directory/static/js/2.8f8731de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95650069e11bbb535a4",
+    "url": "/HW19-React_Directory/static/js/main.7b89f0a9.chunk.js"
+  },
+  {
+    "revision": "bf35f11b4232d101e433",
+    "url": "/HW19-React_Directory/static/js/runtime-main.0f36a503.js"
+  }
+]);
